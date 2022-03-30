36c0edad34cfe65835040c4e97104c204d76ec8a
curiousleo <curiousleo@users.noreply.github.com>
2022-03-30 10:16:18 +0200

    Run pre-commit via poetry

--- commands/cmd_ready_to_rock.py
+++ commands/cmd_ready_to_rock.py
@@ -5,7 +5,7 @@ def cmd_ready_to_rock(args) -> None:
     try:
         subprocess.run(["poetry", "run", "pyright"], check=True)
         subprocess.run(["poetry", "run", "pytest"], check=True)
-        subprocess.run(["pre-commit", "run", "-a"], check=True)
+        subprocess.run(["poetry", "run", "pre-commit", "run", "-a"], check=True)
         rev = subprocess.run(
             ["git", "rev-parse", "HEAD"], capture_output=True, text=True, check=True
         ).stdout.strip()
